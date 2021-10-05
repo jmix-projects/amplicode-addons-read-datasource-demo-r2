22a4f680f319d35e47077957e32960e872a2bd54
Vladislav Selitskii <selitskiivd@haulmont.com>
2021-10-05 14:16:20 +0400

    Fixed code review remarks

--- src/test/groovy/MasterReplicaRoutingDataSourceTest.groovy
+++ src/test/groovy/MasterReplicaRoutingDataSourceTest.groovy
@@ -1,16 +1,19 @@
+import com.haulmont.npaddonsdemor2.dsconfiguration.MasterReplicaRoutingDataSource
+import com.haulmont.npaddonsdemor2.repository.OwnerRepository
 import com.haulmont.npaddonsdemor2.service.OwnerServiceInner
 import com.haulmont.npaddonsdemor2.service.OwnerServiceOuter
+import org.spockframework.spring.EnableSharedInjection
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.transaction.CannotCreateTransactionException
 import spock.lang.Shared
 import spock.lang.Specification
 
 import javax.sql.DataSource
 
 @ContextConfiguration(classes = DemoR2TestConfiguration)
+@EnableSharedInjection
 class MasterReplicaRoutingDataSourceTest extends Specification {
 
     @Autowired
@@ -38,11 +41,14 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
     MasterReplicaRoutingDataSource routingDs
 
     @Shared
+    @Autowired
     List<JdbcTemplate> jdbcTemplates
 
     @Autowired
-    void autowireJdbcTemplates(List<JdbcTemplate> jdbcTemplates) {
-        this.jdbcTemplates = jdbcTemplates
+    OwnerRepository ownerRepository
+
+    void setup() {
+        routingDs.clearSlaveCounter()
     }
 
     def "Test read-write transaction"() {
@@ -50,7 +56,6 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
         def owners = ownerServiceInner.findAll()
 
         then:
-        masterDs == routingDs.determineTargetDataSource()
         !owners.isEmpty()
         "Anton" == owners.get(0).firstName
         "Ivanov" == owners.get(0).lastName
@@ -62,7 +67,6 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
         def owners = ownerServiceInner.findAllReadOnly()
 
         then:
-        slaveDs == routingDs.determineTargetDataSource()
         !owners.isEmpty()
         "Anton_Slave" == owners.get(0).firstName
         "Ivanov_Slave" == owners.get(0).lastName
@@ -71,7 +75,6 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
         owners = ownerServiceInner.findAllReadOnly()
 
         then:
-        slave1Ds == routingDs.determineTargetDataSource()
         !owners.isEmpty()
         "Anton_Slave1" == owners.get(0).firstName
         "Ivanov_Slave1" == owners.get(0).lastName
@@ -80,7 +83,6 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
         owners = ownerServiceInner.findAllReadOnly()
 
         then:
-        masterDs == routingDs.determineTargetDataSource()
         !owners.isEmpty()
         "Anton" == owners.get(0).firstName
         "Ivanov" == owners.get(0).lastName
@@ -89,7 +91,6 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
         owners = ownerServiceInner.findAllReadOnly()
 
         then:
-        slaveDs == routingDs.determineTargetDataSource()
         !owners.isEmpty()
         "Anton_Slave" == owners.get(0).firstName
         "Ivanov_Slave" == owners.get(0).lastName
@@ -97,10 +98,21 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
 
     def "Test nested transactions: read-only -> read-write"() {
         when:
-        ownerServiceOuter.findAllReadOnly()
+        def owners = ownerServiceOuter.findAllReadOnly()
 
         then:
-        thrown CannotCreateTransactionException
+        "Anton_Slave" == owners.get(0).firstName
+        "Ivanov_Slave" == owners.get(0).lastName
+    }
+
+    def "Test nested transactions: read-only -> read-write (propagation: requires_new)"() {
+        when:
+        def owners = ownerServiceOuter.findAllRequiresNew()
+
+        then:
+        noExceptionThrown()
+        "Anton" == owners.get(0).firstName
+        "Ivanov" == owners.get(0).lastName
     }
 
     def "Test nested transactions: read-write -> read-only"() {
@@ -108,7 +120,6 @@ class MasterReplicaRoutingDataSourceTest extends Specification {
         def owners = ownerServiceOuter.findAll()
 
         then:
-        masterDs == routingDs.determineTargetDataSource()
         !owners.isEmpty()
         "Anton" == owners.get(0).firstName
         "Ivanov" == owners.get(0).lastName
